ebb1b71bcc65d851eee322884383077beaf50745
Bryan Galvin <bcgalvin@gmail.com>
2022-10-15 20:04:36 -0700

    update projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -4,9 +4,10 @@ const { awscdk, TextFile } = require('projen');
 
 const cdkVersion = '2.46.0';
 const projenVersion = 'v0.63.23';
+const constructsVersion = '10.1.131';
 const nodejsVersion = 'v16.17.1';
 const commonIgnore = ['.idea', '.Rproj', '.vscode', 'cdk.context.json', '.DS_Store'];
-const deps = [`aws-cdk-lib@${cdkVersion}`, 'constructs@10.0.5', `@aws-cdk/aws-lambda-go-alpha@${cdkVersion}-alpha.0`];
+const deps = [`aws-cdk-lib@${cdkVersion}`, `constructs@${constructsVersion}`];
 
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Bryan Galvin',
@@ -18,15 +19,9 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // Deps
   cdkVersion: cdkVersion,
   projenVersion: projenVersion,
+  constructsVersion: constructsVersion,
   deps: deps,
-  devDeps: [
-    `aws-cdk-lib@${cdkVersion}`,
-    '@types/jest',
-    '@types/node',
-    'eslint-config-prettier',
-    'eslint-plugin-prettier',
-    'prettier',
-  ],
+  devDeps: ['@types/jest', '@types/node', 'eslint-config-prettier', 'eslint-plugin-prettier', 'prettier'],
   // Docs, Testing & Linting
   codeCov: true,
   eslint: true,
